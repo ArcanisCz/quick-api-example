211ae3331b9dcb9481b6464a17d31e6f7ae37b57
ArcanisCz <arcaniscz@gmail.com>
2017-08-07 20:47:50 +0200

    api

--- src/controller.js
+++ src/controller.js
@@ -1,11 +1,109 @@
 import lowdb from "lowdb";
+import {v1} from "uuid";
 
-const db = lowdb('src/db.json');
+const DB = lowdb('db.json');
 
-export const getAll = (req, res) => {
-    const posts = db.get("todos")
+export const getAll = ({}, res) => {
+    const posts = DB.get("todos")
         .sortBy('createdDate')
         .reverse()
         .value();
     res.send(posts);
+};
+
+export const createTodo = ({body}, res) => {
+    if (!body.text) {
+        res.status(422).send("'text' field must be present in json");
+    } else {
+        const written = DB.get('todos')
+            .push({
+                id: v1(),
+                text: body.text,
+                completed: false,
+                createdDate: new Date().getTime()
+            })
+            .last()
+            .write();
+        res.send(written);
+    }
+};
+
+export const deleteTodo = ({param}, res) => {
+    const id = param("id");
+    if (!id) {
+        res.status(422).send("'id' must be present in params");
+    } else {
+        const deleted = DB.get('todos')
+            .remove({id})
+            .last()
+            .write();
+        if (deleted.length === 0) {
+            res.status(404).send("id not found, nothing to delete");
+        } else {
+            res.send(deleted);
+        }
+    }
+};
+
+export const updateText = ({param, body}, res) => {
+    const text = body.text;
+    const id = param("id");
+    if (!text) {
+        res.status(422).send("'text' field must be present in json");
+    } else if (!id) {
+        res.status(422).send("'id' must be present in params");
+    } else {
+        const written = DB.get('todos')
+            .find({id})
+            .assign({text})
+            .last()
+            .write();
+        res.send(written);
+    }
+};
+
+export const complete = ({param}, res) => {
+    const id = param("id");
+    if (!id) {
+        res.status(422).send("'id' must be present in params");
+    } else {
+        const completed = DB.get('todos')
+            .find({
+                id: id,
+                completed: false
+            })
+            .assign({
+                completed: true,
+                completedDate: new Date().getTime()
+            })
+            .write();
+        if (!completed.id) {
+            res.status(404).send("id not found or trying to complete already completed item");
+        } else {
+            res.send(completed);
+        }
+    }
+};
+
+export const incomplete = ({req}, res) => {
+    const id = req.param("id");
+    if (!id) {
+        res.status(422).send("'id' must be present in params");
+    } else {
+        const incompleted = DB.get('todos')
+            .find({
+                id: id,
+                completed: true
+            })
+            .assign({
+                completed: false,
+                completedDate: undefined
+            })
+            .write();
+        if (!incompleted.id) {
+            res.status(404).send("id not found or trying to incomplete not completed item");
+        } else {
+            res.send(incompleted);
+        }
+    }
 };
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,24 @@
-import {getAll} from "./controller";
+import {
+    getAll,
+    createTodo,
+    deleteTodo,
+    updateText,
+    complete,
+    incomplete,
+} from "./controller";
 
 export default (app) => {
     app.route('/')
         .get(getAll)
+        .post(createTodo);
+
+    app.route("/:id")
+        .post(updateText)
+        .delete(deleteTodo);
+
+    app.route("/todos/:id/complete")
+        .post(complete);
+
+    app.route("/todos/:id/incomplete")
+        .post(incomplete);
 };
